572af7ae53d443866460976309aacf8063c8e749
sMorrison26 <morris10@rpi.edu>
2022-11-24 23:00:48 -0500

    Home
    Constructing the home page

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    //Load the json file into the main div
+    //hide on load
+    $("#charitiesLoaded").hide();
+    $("#charitySearch").click( function() {
+        //do something when 
+    });
+
+});
