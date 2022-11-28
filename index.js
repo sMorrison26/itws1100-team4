bded93cfa4eb5f718c59e2198a7bdf3762caa989
sMorrison26 <morris10@rpi.edu>
2022-11-28 02:01:57 -0500

    Revising Website Layout
    Reevaluated the website design and layout, changed the color theme, and read JSON file to load in charity data.

--- index.js
+++ index.js
@@ -2,9 +2,44 @@ $(document).ready(function () {
 
     //Load the json file into the main div
     //hide on load
-    $("#charitiesLoaded").hide();
-    $("#charitySearch").click( function() {
-        //do something when 
+    $.ajax({
+        type: 'GET',
+        url: 'json/team4.json',
+        dataType: 'json',
+        success: function (responseData, status){
+            $.each(responseData.Charity, function () {
+                var output = '</br><div class="charity"><h2>'+this.name+
+                '</h2><p>Type: '+this.type+'</p></br><p>Link: </p><a href="'+this.url+'">'+this.name+'</a></p></div></br>';
+                switch (this.type){
+                    case "International Needs":
+                        $("#International").append(output);
+                        break;
+                    case "Domestic Needs":
+                        $("#Domestic").append(output);
+                        break;
+                    case "Medical":
+                        $("#Domestic").append(output);
+                        break;
+                    case "Environment":
+                        $("#Domestic").append(output);
+                        break;
+                    case "Religious":
+                        $("#Domestic").append(output);
+                        break;
+                    case "Health":
+                        $("#Domestic").append(output);
+                        break;
+                    case "Youth":
+                        $("#Domestic").append(output);
+                        break;
+                    default: 
+                        break;
+                    }
+                });
+            }
+        });
+    // $("#Discover").hide();  
+    $("#Discover").click( function() {
+        $("#Discover").toggle();
     });
-
 });
